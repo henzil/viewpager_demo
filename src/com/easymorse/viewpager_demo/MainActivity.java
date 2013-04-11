e689280385c6512c68c27190a7723b1b724ad369
lizhen <lizhen@witmob.com>
2013-04-11 14:09:51 +0800

    完整版本的viewpager 加入类似google play效果。

--- src/com/easymorse/viewpager_demo/MainActivity.java
+++ src/com/easymorse/viewpager_demo/MainActivity.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,8 @@ public class MainActivity extends Activity {
 	private PagerTabStrip pagetab;
 	
 	private List<Map<?, ?>> list = new ArrayList<Map<?, ?>>();
+	
+	private MyViewPagerAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,15 @@ public class MainActivity extends Activity {
 	private void initView(){
 		viewPager = (ViewPager) findViewById(R.id.viewpager);
 		pagetab = (PagerTabStrip) findViewById(R.id.pagetab);
-		
+		adapter = new MyViewPagerAdapter(this);
+		viewPager.setAdapter(adapter);
+		adapter.setData(list);
+		pagetab.setTabIndicatorColor(Color.BLUE);  // title下横线颜色值。
+		pagetab.setDrawFullUnderline(false); //不显示整条横线。
+		pagetab.setBackgroundColor(Color.RED); //背景
+//		pagetab.setTextSpacing(10); //
+		pagetab.setTextColor(Color.YELLOW);
+		pagetab.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);//设置字体大小。
 	}
 	
 	private void initData() {
@@ -45,17 +57,17 @@ public class MainActivity extends Activity {
 
 		map = new HashMap<Object,Object>();
 		map.put("drawableId", R.drawable.c);
-		map.put("name", "我是史蒂夫vsdfdfssd曹磊");
+		map.put("name", "我是史蒂夫vsdfdfssd");
 		list.add(map);
 
 		map = new HashMap<Object,Object>();
 		map.put("drawableId", R.drawable.d);
-		map.put("name", "我是曹磊我是曹磊我是曹磊");
+		map.put("name", "4");
 		list.add(map);
 
 		map = new HashMap<Object,Object>();
 		map.put("drawableId", R.drawable.e);
-		map.put("name", "我是曹磊我是曹磊我是曹磊");
+		map.put("name", "5");
 		list.add(map);
 	}
 
